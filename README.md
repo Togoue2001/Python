a4a7d525166765593a597658a33434b707eb55b6
Togoue2001 <dylanetogoue@gmail.com>
2025-11-05 11:35:02 +0100

    ajout du fichier Readme et gitignore

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Python
-GestionMagasin
+# gestion_magasin.py
+"""
+SYSTÈME DE GESTION DE MAGASIN - CLI
+Fonctionnalités:
+ - Produits : ajouter / modifier / supprimer / lister / rechercher / ruptures
+ - Clients  : ajouter / lister / rechercher / total_achats
+ - Ventes   : enregistrer vente, historique, reçu
+ - Statistiques : CA total, produit le plus vendu, meilleur client, stock total, ventes par catégorie
+ - Sauvegarde JSON automatique après chaque modification et chargement au démarrage
+"""
--- gestion_magasin.py
+++ gestion_magasin.py
@@ -1,13 +1,3 @@
-# gestion_magasin.py
-"""
-SYSTÈME DE GESTION DE MAGASIN - CLI
-Fonctionnalités:
- - Produits : ajouter / modifier / supprimer / lister / rechercher / ruptures
- - Clients  : ajouter / lister / rechercher / total_achats
- - Ventes   : enregistrer vente, historique, reçu
- - Statistiques : CA total, produit le plus vendu, meilleur client, stock total, ventes par catégorie
- - Sauvegarde JSON automatique après chaque modification et chargement au démarrage
-"""
 import json
 from datetime import datetime
 from copy import deepcopy
